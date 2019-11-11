7b686d85fc23981a541e468ab2bd3f9122a1fd8a
curic4t <curic4t@gmail.com>
2019-11-11 14:32:55 +0900

    '1111'

---
+++ Server/Rollingpaasta/src/main/java/com/example/demo/DTO/LightDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.DTO;
+
+public class LightDTO {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
---
+++ Server/Rollingpaasta/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ko" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Hello</title>
+</head>
+
+<body>
+  <h1>Hello World with jsp</h1>
+</body>
+</html>

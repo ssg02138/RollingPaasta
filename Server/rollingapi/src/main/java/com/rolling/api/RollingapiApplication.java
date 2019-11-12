893240a907773588999c3c572aad17e64327b9fa
KBS <ssg02138@naver.com>
2019-11-13 01:17:00 +0900

    Setting Complete

---
+++ Server/rollingapi/src/main/java/com/rolling/api/RollingapiApplication.java
@@ -0,0 +1,13 @@
+package com.rolling.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollingapiApplication.class, args);
+	}
+
+}
---
+++ Server/rollingapi/src/test/java/com/rolling/api/RollingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolling.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
